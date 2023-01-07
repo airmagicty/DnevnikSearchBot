b1580b0019fe86ecd0f6a6eb411886b152f34680
airmagicty <airmagicty@gmail.com>
2023-01-07 21:06:42 +0300

    Сделал бота со стикерами и хелпой + /commands > bot.py сделал команды /help /start /restart и отправку стикера в ответ на любое сообщение > CONFIG.py описание HELP_LIST для /help

--- CONFIG.py
+++ CONFIG.py
@@ -1,2 +1,7 @@
 API_TOKEN = '5840846299:AAGW2BfGVd9z-vG-jmYzLftMeGEC4Athe1w'
 ADMIN_LIST = ['5840846299']
+
+HELP_LIST = """
+/start, /restart \- *Запуск/перезапуск бота*
+/help \- *Инструкция по использованию*
+"""
--- bot.py
+++ bot.py
@@ -1,12 +1,24 @@
+import re
+import random
 from aiogram import Bot, Dispatcher, executor, types
 from CONFIG import *
 
 bot = Bot(API_TOKEN)
 dp = Dispatcher(bot)
 
+alfavit = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+
 @dp.message_handler()
 async def echo(message: types.Message):
+    await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEHKgZjubMISJ0NWmCOascFkOue0MngegACcAEAAs6YzRbDY97SastfMC0E')
+
+@dp.message_handler(commands=['start','restart'])
+async def echo_start(message: types.Message):
     await message.answer(text=message.text)
+
+@dp.message_handler(commands=['help'])
+async def echo_help(message: types.Message):
+    await message.reply(text=HELP_LIST, parse_mode='MarkdownV2')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
