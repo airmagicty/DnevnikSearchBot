f1d2d51071c28599fa575e902a345d4b62b92c95
airmagicty <airmagicty@gmail.com>
2023-01-08 20:52:03 +0300

    Клавиатура > bot.py call_back_return - вызов кнопкой test_imenu - меню объект on_bot_start - стартовое сообщение echo_menu - менюшка создание

--- CONFIG.py
+++ CONFIG.py
@@ -2,7 +2,7 @@ API_TOKEN = '5840846299:AAGW2BfGVd9z-vG-jmYzLftMeGEC4Athe1w'
 ADMIN_LIST = ['5840846299']
 
 HELP_LIST = """
-/start /restart /help /sticker /locate /img
+/start /restart /help /sticker /img /menu
 """
 
 
--- bot.py
+++ bot.py
@@ -1,34 +1,65 @@
 import re
-import random
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from CONFIG import *
 
 bot = Bot(API_TOKEN)
 dp = Dispatcher(bot)
 
-def create_menu():
-    return 0
+def call_back_return():
+    print("Hello!")
+def test_menu() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardMarkup(resize_keyboard=True,
+                             one_time_keyboard=True)
+    kb1 = KeyboardButton("Авт1")
+    kb2 = KeyboardButton("Авт2")
+    kb3 = KeyboardButton("/help")
+    return kb.add(kb1).insert(kb2).add(kb3)
+
+def test_imenu() -> InlineKeyboardMarkup:
+    ikb = InlineKeyboardMarkup(row_width=2)
+    ikb1 = InlineKeyboardButton(text="Test1",
+                                url="https://google.com")
+    ikb2 = InlineKeyboardButton(text="Test2",
+                                url="https://google.com")
+    ikb3 = InlineKeyboardButton(text="Test3",
+                                callback_data=call_back_return())
+    return ikb.add(ikb1, ikb2).add(ikb3)
+
+async def on_bot_start(_):
+    print("I started")
+
+@dp.message_handler(commands=['menu'])
+async def echo_menu(message: types.Message):
+    await bot.send_message(chat_id=message.from_user.id,
+                           text="Menu",
+                           reply_markup=test_imenu()
+                           )
 
 @dp.message_handler(commands=['img'])
 async def echo_img(message: types.Message):
-    await bot.send_photo(chat_id=message.from_user.id, photo=IMG_URL)
-
-@dp.message_handler(commands=['locate'])
-async def echo_locate(message: types.Message):
-    await bot.send_location(message.from_user.id)
+    await bot.send_photo(chat_id=message.from_user.id,
+                         photo=IMG_URL)
 
 @dp.message_handler(commands=['sticker'])
 async def echo_sticker(message: types.Message):
-    await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEHKgZjubMISJ0NWmCOascFkOue0MngegACcAEAAs6YzRbDY97SastfMC0E')
+    await bot.send_sticker(chat_id=message.from_user.id,
+                           sticker='CAACAgIAAxkBAAEHKgZjubMISJ0NWmCOascFkOue0MngegACcAEAAs6YzRbDY97SastfMC0E',
+                           reply_markup=ReplyKeyboardRemove())
 
 @dp.message_handler(commands=['start','restart'])
 async def echo_start(message: types.Message):
-    await message.answer(text=message.text)
-    await message.delete()
+    await bot.send_message(chat_id=message.from_user.id,
+                           text="Test",
+                           parse_mode='HTML',
+                           reply_markup=test_menu())
 
 @dp.message_handler(commands=['help'])
 async def echo_help(message: types.Message):
     await message.reply(text=HELP_LIST, parse_mode='MarkdownV2')
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dispatcher=dp,
+                           skip_updates=True,
+                           on_startup=on_bot_start)
