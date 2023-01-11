046ac4d47f6ae574c960d15838a519b3c63d1e36
airmagicty <airmagicty@gmail.com>
2023-01-11 18:19:38 +0300

    Тестовый push

--- bot.py
+++ bot.py
@@ -3,12 +3,11 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from CONFIG import *
+from TOKEN import API_TOKEN
 
 bot = Bot(API_TOKEN)
 dp = Dispatcher(bot)
 
-def call_back_return():
-    print("Hello!")
 def test_menu() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardMarkup(resize_keyboard=True,
                              one_time_keyboard=True)
@@ -21,14 +20,11 @@ def test_imenu() -> InlineKeyboardMarkup:
     ikb = InlineKeyboardMarkup(row_width=2)
     ikb1 = InlineKeyboardButton(text="Test1",
                                 url="https://google.com")
-    ikb2 = InlineKeyboardButton(text="Test2",
-                                url="https://google.com")
+    ikb2 = InlineKeyboardButton(text="nTest2",
+                                callback_data="test")
     ikb3 = InlineKeyboardButton(text="Test3",
                                 callback_data="summon_test")
-    return ikb.add(ikb1, ikb2).add(ikb3)
-
-async def on_bot_start(_):
-    print("I started")
+    return ikb.add(ikb1).add(ikb2, ikb3)
 
 @dp.message_handler(commands=['menu'])
 async def echo_menu(message: types.Message):
@@ -59,7 +55,25 @@ async def echo_start(message: types.Message):
 async def echo_help(message: types.Message):
     await message.reply(text=HELP_LIST, parse_mode='MarkdownV2')
 
+@dp.callback_query_handler()
+async def call_back_return(callback: types.CallbackQuery):
+    if callback.data == 'summon_test':
+        await bot.delete_message(chat_id=callback.message.chat.id,
+                             message_id=callback.message.message_id)
+        await bot.send_message(chat_id=callback.message.chat.id,
+                               text=HELP_LIST,
+                               parse_mode='MarkdownV2')
+    await callback.answer("Засуммонился")
+
+
+async def on_bot_start(_):
+    print("I started")
+
+async def on_bot_stop(_):
+    print("I stopped")
+
 if __name__ == '__main__':
     executor.start_polling(dispatcher=dp,
                            skip_updates=True,
-                           on_startup=on_bot_start)
+                           on_startup=on_bot_start,
+                           on_shutdown=on_bot_stop)
